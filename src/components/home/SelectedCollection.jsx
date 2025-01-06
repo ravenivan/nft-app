a2ff85090c2b2109b80af5e90a63efb88e832c93
Ivan Lam <ravenivan1@gmail.com>
2025-01-05 20:24:55 -0500

    Selected Collections finished

--- src/components/home/SelectedCollection.jsx
+++ src/components/home/SelectedCollection.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import SelectedItemVideo from "../../assets/selected-collection.mp4";
 import SelectedItemThumbnail from "../../assets/selected-collection-thumbnail.jpg";
 import SelectedItemLogo from "../../assets/selected-collection-logo.avif";
 import VerifiedIcon from "../../assets/verified.png";
 import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import Skeleton from "../ui/Skeleton";
 
 export default function SelectedCollection() {
+
+  const { fetchData } = useContext(AppContext)
+
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchSelectedCollection = async () => {
+      try {
+        const fetchedData = await fetchData("https://remote-internship-api-production.up.railway.app/selectedCollection")
+        setData(fetchedData)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+
+    fetchSelectedCollection()
+  }, [])
+
+  useEffect(() => {
+    console.log(data)
+  }, [data])
+
   return (
     <header>
-      <div className="selected-collection">
+      {data ? (
+        <div className="selected-collection">
         <video
           autoPlay
           muted
           loop
           playsInline
-          poster={SelectedItemThumbnail}
-          src={SelectedItemVideo}
+          poster={data.thumbnail}
+          src={data.videoLink}
           className="selected-collection__bg"
         />
         <div className="selected-collection__description">
@@ -25,22 +50,27 @@ export default function SelectedCollection() {
             className="selected-collection__logo"
           />
           <h1 className="selected-collection__title">
-            Coachella Throwback Merchandise Trunk
+            {data.title}
           </h1>
           <Link to={'/user'} className="selected-collection__author">
-            By Coachella
+            By {data.creator}
             <img
               src={VerifiedIcon}
               className="selected-collection__author__verified"
             />
           </Link>
-          <div className="selected-collection__details">10 items · 3.4 ETH</div>
+          <div className="selected-collection__details">{data.amountOfItems} items · {data.floorPrice} ETH</div>
           <Link to={'/collection'} className="selected-collection__button">
             <div className="green-pulse"></div>
             View Collection
           </Link>
         </div>
       </div>
+      ) : (
+        <div className="selected-collection">
+          <Skeleton width={"100%"} height={"100%"} border-radius={"0px"} />
+        </div>
+      )}
     </header>
   );
 }
